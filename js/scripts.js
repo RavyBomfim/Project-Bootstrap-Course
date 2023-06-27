9c180f4fb0407521d63f1613345b7935df57c1a7
Ravy Bomfim <87732549+RavyBomfim@users.noreply.github.com>
2023-06-27 03:34:47 -0300

    Finished project

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     // Progress bar
+
     let containerA = document.getElementById('circleA');
 
     let circleA = new ProgressBar.Circle(containerA, {
@@ -89,6 +90,7 @@ $(document).ready(function() {
     });
 
     // Iniciando o loader quando o usuário chegar no elemento
+
     let dataAreaOffset = $('#data-area').offset();
     let stop = 0;
 
@@ -110,6 +112,7 @@ $(document).ready(function() {
     });
 
     // Parallax
+
     setTimeout(function() {
 
         $('#data-area').parallax({imageSrc: 'images/cidadeparallax.png'});
@@ -117,5 +120,79 @@ $(document).ready(function() {
         $('#apply-area').parallax({imageSrc: 'images/pattern.png'});
 
     }, 250);
+
+    // Filtro do Portfólio
+    
+    $('.filter-btn').on('click', function() {
+
+        let type = $(this).attr('id');
+        let boxes = $('.project-box');
+
+        $('.main-btn').removeClass('active');
+        $(this).addClass('active');
+
+        if (type == 'dev-btn') {
+            eachBoxes('dev', boxes);
+        } else if (type == 'dsg-btn') {
+            eachBoxes('dsg', boxes);
+        } else if (type == 'seo-btn') {
+            eachBoxes('seo', boxes);
+        } else {
+            eachBoxes('all', boxes);
+        }
+
+    });
+
+    function eachBoxes(type, boxes) {
+
+        if (type == 'all') {
+            $(boxes).fadeIn();
+        } else {
+            $(boxes).each(function() {
+                if(!$(this).hasClass(type)) {
+                    $(this).fadeOut('slow');
+                } else {
+                    $(this).fadeIn();
+                }
+            });
+        }
+    }
+
+    //Scroll para seções
+
+    let navBtn = $('.nav-item');
+
+    let bannerSection = $('#mainSlider');
+    let aboutSection = $('#about-area');
+    let servicesSection = $('#services-area');
+    let teamSection = $('#team-area');
+    let portfolioSection = $('#portfolio-area');
+    let contactSection = $('#contact-area');
+
+    let scrollTo = '';
+
+    $(navBtn).click(function() {
+
+        let btnId = $(this).attr('id');
+
+        if (btnId == 'about-menu') {
+            scrollTo = aboutSection;
+        } else if (btnId == 'services-menu') {
+            scrollTo = servicesSection;
+        } else if(btnId == 'team-menu') {
+            scrollTo = teamSection;
+        } else if(btnId == 'portfolio-menu') {
+            scrollTo = portfolioSection;
+        } else if(btnId == 'contacts-menu') {
+            scrollTo = contactSection;
+        } else {
+            scrollTo = bannerSection;
+        }
+
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(scrollTo).offset().top - 70
+        }, 500);
+
+    });
 
 });
